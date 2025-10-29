8a76e28cca35e3be227b7ced23fe3be6cbca81e1
suzuki-aki-rin <136300872+suzuki-aki-rin@users.noreply.github.com>
2025-10-30 00:34:56 +0900

    Create adjust_window_geometry.sh
    To set window geometry for capture before recording.

---
+++ adjust_window_geometry.sh
@@ -0,0 +1,5 @@
+# Adjust location and size of game window.
+# An example of Petal Crash
+# -r "Window Title"
+# -e 0,x,y,width,height: sets the geometry of the window.
+wmctrl -r "Petal Crash" -e 0,1000,0,1920,1080
